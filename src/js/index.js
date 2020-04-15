8551f30278ff31b311f2a2d36602e991b7cdaa33
CodeSigils <geotom.dev@gmail.com>
2020-04-15 23:10:52 +0300

    add purgecss and cssnano

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,12 @@
+// Font-awesome
+// import '@fortawesome/fontawesome-free/css/all.css'
+
 // Setup styles
-import '/scss/main.scss';
+import '../scss/main.scss'
 
 // Setup jquery
-import $ from 'jquery';
-window.$ = window.jQuery = $;
+import $ from 'jquery'
+window.$ = window.jQuery = $
 
 // Setup framework scripts
-import 'bootstrap';
+import 'bootstrap'
